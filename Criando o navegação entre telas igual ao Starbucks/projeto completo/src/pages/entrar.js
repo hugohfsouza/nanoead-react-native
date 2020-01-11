d15354f07dddf897df224baea1cce424a1d8627d
Hugo Fumero <hugohfsouza@gmail.com>
2020-01-11 11:28:42 -0300

    inserindo o projeto criando a navegação entre telas igual ao starbucks

---
+++ Criando o navegação entre telas igual ao Starbucks/projeto completo/src/pages/entrar.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+export default class Entrar extends Component{
+    render(){
+        return(
+            <View>
+                <Text>Tela de Entrar</Text>
+            </View>
+        );
+    }
+}
