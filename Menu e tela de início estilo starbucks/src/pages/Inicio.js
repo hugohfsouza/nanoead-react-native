227eabeaaec886f4af7187f49402ff1c272a14f9
Hugo Fumero <hugohfsouza@gmail.com>
2020-01-11 08:28:20 -0300

    projeto Menu e tela de início estilo starbucks

---
+++ Menu e tela de início estilo starbucks/src/pages/Inicio.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {View, Text } from 'react-native';
+
+export default class Inicio extends Component{
+
+    render(){
+        return(
+            <View>
+                <Text>Tela de Inicio</Text>
+            </View>
+        );
+    }
+}
